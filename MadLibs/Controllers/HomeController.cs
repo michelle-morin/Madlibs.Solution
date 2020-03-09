33f0cb103be7a84cde8d150c508d35dfe8f03572
Antonio Cruz and Michelle Morin <antonio@>
2020-03-09 11:20:07 -0700

    update home controller to include another form and another madlib story, rename existing form and story

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,25 +5,43 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/form")]
-    public ActionResult Form()
+    [Route("/")]
+    public ActionResult Options()
     {
       return View();
     }
 
-    [Route("/story")]
-    public ActionResult Story(string time, string name, string drink, string verb, string transportation, string noun, string alttransportation, string exclamation) 
+    [Route("/epicodus")]
+    public ActionResult Epicodus()
     {
-      StoryWords myStoryWords = new StoryWords();
-      myStoryWords.Time = time;
-      myStoryWords.Name = name;
-      myStoryWords.Drink = drink;
-      myStoryWords.Verb = verb;
-      myStoryWords.Transportation = transportation;
-      myStoryWords.Noun = noun;
-      myStoryWords.AltTransportation = alttransportation;
-      myStoryWords.Exclamation = exclamation;
-      return View(myStoryWords); 
+      return View();
+    }
+
+    [Route("/epicodus-story")]
+    public ActionResult EpicodusStory(string time, string name, string drink, string verb, string transportation, string noun, string alttransportation, string exclamation) 
+    {
+      EpicodusWords myEpicodusWords = new EpicodusWords();
+      myEpicodusWords.Time = time;
+      myEpicodusWords.Name = name;
+      myEpicodusWords.Drink = drink;
+      myEpicodusWords.Verb = verb;
+      myEpicodusWords.Transportation = transportation;
+      myEpicodusWords.Noun = noun;
+      myEpicodusWords.AltTransportation = alttransportation;
+      myEpicodusWords.Exclamation = exclamation;
+      return View(myEpicodusWords); 
+    }
+
+    [Route("/trip")]
+    public ActionResult Trip()
+    {
+      return View();
+    }
+
+    [Route("/trip-story")]
+    public ActionResult TripStory()
+    {
+
     }
   }
 }
