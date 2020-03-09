ded12f9578a643474f0be86f24627eaea7f9a5c9
Antonio Cruz and Michelle Morin <michelle@>
2020-03-09 10:50:35 -0700

    model for storywords

---
+++ MadLibs/Models/StoryWords.cs
@@ -0,0 +1,14 @@
+namespace MadLibs.Models
+{
+  public class StoryWords
+  {
+    public string Time { get; set; }
+    public string Name { get; set; }
+    public string Drink { get; set; }
+    public string Verb { get; set; }
+    public string Transportation { get; set; }
+    public string Noun { get; set; }
+    public string AltTransportation { get; set; }
+    public string Exclamation { get; set; }
+  }
+}
